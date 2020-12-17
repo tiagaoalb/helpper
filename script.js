2b529f00c9ab768b0859e0bf69d24984b5c0faf7
Tiago <tiagaoalb@gmail.com>
2020-12-17 15:45:17 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,42 @@
+const selectStates = document.getElementById('state');
+
+const statesOptions = {
+	AC: 'Acre',
+	AL: 'Alagoas',
+	AP: 'Amapá',
+	AM: 'Amazonas',
+	BA: 'Bahia',
+	CE: 'Ceará',
+	DF: 'Distrito Federal',
+	ES: 'Espírito Santo',
+	GO: 'Goiás',
+	MA: 'Maranhão',
+	MT: 'Mato Grosso',
+	MS: 'Mato Grosso do Sul',
+	MG: 'Minas Gerais',
+	PA: 'Pará',
+	PB: 'Paraíba',
+	PR: 'Paraná',
+	PE: 'Pernambuco',
+	PI: 'Piauí',
+	RJ: 'Rio de Janeiro',
+	RN: 'Rio Grande do Norte',
+	RS: 'Rio Grande do Sul',
+	RO: 'Rondônia',
+	RR: 'Roraima',
+	SC: 'Santa Catarina',
+	SP: 'São Paulo',
+	SE: 'Sergipe',
+	TO: 'Tocantins',
+};
+
+function setStateOption() {
+	for (key in statesOptions) {
+		let value = statesOptions[key];
+		const createOption = document.createElement('option');
+		createOption.value = [key];
+		createOption.innerText = value;
+		selectStates.appendChild(createOption);
+	}
+}
+setStateOption();
